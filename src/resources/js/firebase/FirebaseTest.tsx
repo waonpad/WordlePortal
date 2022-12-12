544cfad1b0a16539fd8793eec05217acf3097c8b
waonpad <waonpad@gmail.com>
2022-12-12 18:00:01 +0900

    1212

--- src/resources/js/firebase/FirebaseTest.tsx
+++ src/resources/js/firebase/FirebaseTest.tsx
@@ -7,14 +7,25 @@ import Button from '@mui/material/Button';
 
 import firebaseApp from "../contexts/FirebaseConfig";
 import { getDatabase, push, ref, set, update, onValue, onDisconnect, child, orderByChild, equalTo, startAt, endAt } from '@firebase/database'
+import { serverTimestamp } from 'firebase/database';
 import { useAuth } from '../contexts/AuthContext';
 import { useParams } from 'react-router-dom';
 
-type data = {
+type Posts = {
     [key: string]: {
         name: string
         msg: string
+        time: any
     }
+}
+
+type FirebaseGameData = {
+    posts?: Posts[]
+    users?: any
+    host?: any,
+    current_order?: number
+    time_limit?: number
+    status?: 'wait' | 'start' | 'end'
 }
 
 function FirebaseTest(): React.ReactElement {
@@ -22,11 +33,14 @@ function FirebaseTest(): React.ReactElement {
     const auth = useAuth();
     const {room_id} = useParams<{room_id: string}>();
 
-    const [data, setData] = useState<data[]>([]);
+    const [data, setData] = useState<FirebaseGameData>();
     const [data_loading, setDataLoading] = useState<boolean>(true);
+    const [posts, setPosts] = useState<Posts[]>([]);
 
+    const ref = firebaseApp.database().ref(`connections/rooms/${room_id}`);
     const [name, setName] = useState<string>('');
     const [msg, setMsg] = useState<string>('');
+    const [counter, setCounter] = useState<number>();
 
     const handleChangeInputName = (event: any) => {
         setName(event.currentTarget.value);
@@ -37,31 +51,31 @@ function FirebaseTest(): React.ReactElement {
     }
 
     const handleSubmit = (event: any) => {
-        const data = {
-            name: name,
-            msg: msg
-        }
-        
         // データ追加
-        firebaseApp.database().ref('sample').push(data);
+        ref.child('posts').push({
+            name: name,
+            msg: msg,
+            time: serverTimestamp()
+        });
     }
 
     const handleRemoveItem = (event: any) => {
         const key = event.currentTarget.getAttribute('data-key');
 
         // データ削除
-        firebaseApp.database().ref('sample').child(key).remove();
+        ref.child('posts').child(key).remove();
     }
 
     const handleUpdateItemUnchi = (event: any) => {
         const key = event.currentTarget.getAttribute('data-key');
 
-        const prev_item = data[key];
+        const prev = data!.posts![key];
 
         // データ更新
-        firebaseApp.database().ref('sample').child(key).update({
-            ...prev_item,
-            msg: 'うんち！'
+        ref.child('posts').child(key).update({
+            ...prev,
+            msg: 'うんち！',
+            time: serverTimestamp()
         });
     }
 
@@ -74,9 +88,7 @@ function FirebaseTest(): React.ReactElement {
     
     useEffect(() => {
         const connected_info_ref = firebaseApp.database().ref(".info/connected");
-        
         const user_id = auth!.user!.id.toString();
-        const ref = firebaseApp.database().ref(`connections/rooms/${room_id}`);
     
         const connect = async () => {
             // 接続状態を監視する
@@ -90,12 +102,12 @@ function FirebaseTest(): React.ReactElement {
                 }
         
                 // 切断されたら接続情報を削除する処理を予約する
-                await ref.child(`users/${user_id}`).onDisconnect().update({
+                await ref.child(`users/u${user_id}`).onDisconnect().update({
                     status: 'disconnect'
                 });
 
                 // 参加中のルームを設定する
-                await ref.child(`users/${user_id}`).set({
+                await ref.child(`users/u${user_id}`).set({
                     status: 'connect'
                 });
             });
@@ -103,35 +115,52 @@ function FirebaseTest(): React.ReactElement {
     
         connect().catch(() => {
         });
-        
-        // // ルーム内のデータの変更を監視
-        // ref.on('value', (snapshot) => {
-        //     if (snapshot.exists()) {
-        //         console.log(snapshot.val());
-        //         setData(snapshot.val());
-        //     }
-        //     else {
-        //         console.log("No data available");
-        //     }
-        // })
 
-        // setDataLoading(false);
-    }, []);
-
-    useEffect(() => {
         // データの変更を監視
-        firebaseApp.database().ref('sample').on('value', (snapshot) => {
+        ref.on('value', (snapshot) => {
             if (snapshot.exists()) {
                 console.log(snapshot.val());
                 setData(snapshot.val());
+                setPosts(snapshot.val().posts);
+
+                setDataLoading(false);
             }
             else {
                 console.log("No data available");
             }
         })
+    }, []);
 
-        setDataLoading(false);
-    }, [])
+    // 新しい入力があったときに発火させる
+    // 妥協してクライアント側でカウントダウンする
+    // useEffect(() => {
+    //     console.log('reset counter');
+    //     setCounter(30);
+
+    //     const timer = setInterval(() => {
+    //         setCounter(counter => counter! - 1);
+    //     }, 1000)
+
+    //     return () => {
+    //         clearInterval(timer);
+    //         console.log('clear counter');
+    //     }
+    // }, [posts]);
+
+    // useEffect(() => {
+    //     console.log(counter);
+    //     // 指定時間更新が無かったら
+    //     // ホストユーザーがログを更新する責務を負う
+    //     // TODO: ホストが切断された時のホスト変更処理 どういう優先順位でホストを付与する？
+    //     // if(counter === 0 && auth!.user!.id === data!.host) {
+    //     if(counter === 0) {
+    //         ref.child('posts').push({
+    //             name: '',
+    //             msg: '',
+    //             time: serverTimestamp()
+    //         })
+    //     }
+    // }, [counter]);
 
     return (
         <React.Fragment>
@@ -152,12 +181,13 @@ function FirebaseTest(): React.ReactElement {
             >
                 Submit
             </Button>
+            <Typography>{counter}</Typography>
             {
                 !data_loading ? (
                     <Grid container>
-                        {Object.keys(data).map((key: any, index) => (
+                        {Object.keys(data!.posts!).map((key: any, index) => (
                             <Grid item xs={12} key={index} sx={{display: 'flex'}}>
-                                <Typography>{data[key].name} : {data[key].msg}</Typography>
+                                <Typography>{new Date((data!.posts![key].time as any)).toLocaleString()} : {data!.posts![key].name} : {data!.posts![key].msg}</Typography>
                                 <Button
                                     data-key={key}
                                     onClick={handleRemoveItem}
